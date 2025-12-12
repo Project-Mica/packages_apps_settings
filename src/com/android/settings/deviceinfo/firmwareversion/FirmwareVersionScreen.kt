32936821ff671528948ed13cfd94e5d612075b38
aswin7469 <aswinas@pixysos.com>
2025-12-12 18:15:36 +0530

    Revert "Settings: Drop GPSU entry from firmware version"
    This reverts commit 5db51aa7c01fb2d78536a65b6b26b5f79e09c5ed.

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
@@ -72,6 +72,7 @@ open class FirmwareVersionScreen : PreferenceScreenMixin, PreferenceSummaryProvi
         preferenceHierarchy(context) {
             +FirmwareVersionDetailPreference()
             +SecurityPatchLevelPreference()
+            +MainlineModuleVersionPreference()
             +MicaVersionPreference()
             +BasebandVersionPreference()
             +KernelVersionPreference()
